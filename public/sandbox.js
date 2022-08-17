1e309f71e3b899868610e57a7a127062e694362e
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-17 20:53:05 +0530

    type alias

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,45 +1,20 @@
 "use strict";
-let greet = () => {
-    console.log('Hello world');
+;
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid ${uid}`);
 };
-// greet = 'hello'
-// src/sandbox.ts:5:1 - error TS2322: Type 'string' is not assignable to type '() => void'.
-// 5 greet = 'hello'
-let greetOne;
-greet = () => {
-    console.log('Hllo Again');
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-const add = (a, b) => {
-    console.log(a + b);
+const greetAgain = (user) => {
+    console.log(`${user.name} says hello`);
 };
-add(5, 10);
-const addOne = (a, b) => {
-    console.log(a + b);
+const logDetailsOne = (uid, item) => {
+    console.log(`${item} has a uid ${uid}`);
 };
-// addOne(5,'10')
-// src/sandbox.ts:26:10 - error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
-// 26 addOne(5,'10')
-const addTwo = (a, b, c) => {
-    console.log(a + b);
-    console.log(c);
+const greetOne = (user) => {
+    console.log(`${user.name} says hello`);
 };
-addTwo(5, 10);
-const addThree = (a, b, c = 10) => {
-    console.log(a + b);
-    console.log(c);
+const greetAgainOne = (user) => {
+    console.log(`${user.name} says hello`);
 };
-addThree(5, 10);
-addThree(5, 10, 20);
-addThree(5, 10, '20');
-const minus = (a, b) => {
-    return a + b;
-};
-let result = minus(10, 7);
-// result = 'soething else'
-// src/sandbox.ts:53:1 - error TS2322: Type 'string' is not assignable
-// to type 'number'.
-// 53 result = 'soething else'
-const minusOne = (a, b) => {
-    return a + b;
-};
-let resultOne = minusOne(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,65 +1,26 @@
-let greet = () => {
-    console.log('Hello world');
+type StringOrNum = string | number;;
+type objWithName = {name: string, id: StringOrNum}
+
+const logDetails = (uid: string | number, item: string) => {
+    console.log(`${item} has a uid ${uid}`)
 }
 
-// greet = 'hello'
-// src/sandbox.ts:5:1 - error TS2322: Type 'string' is not assignable to type '() => void'.
-
-// 5 greet = 'hello'
-
-let greetOne: Function;
-
-greet = () => {
-    console.log('Hllo Again')
+const greet = (user: { name: string, uid: string | number }) => {
+    console.log(`${user.name} says hello`)
 }
 
-const add = (a:number, b:number) => {
-    console.log(a+b)
+const greetAgain = (user: { name: string, uid: string | number }) => {
+    console.log(`${user.name} says hello`)
 }
 
-add(5,10)
-
-const addOne = (a:number, b:number) => {
-    console.log(a+b)
+const logDetailsOne = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has a uid ${uid}`)
 }
 
-// addOne(5,'10')
-// src/sandbox.ts:26:10 - error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
-
-// 26 addOne(5,'10')
-
-
-const addTwo = (a: number, b: number, c?: number | string) => {
-    console.log(a + b)
-    console.log(c)
+const greetOne = (user:objWithName) => {
+    console.log(`${user.name} says hello`)
 }
 
-addTwo(5,10)
-
-const addThree = (a: number, b: number, c: number | string = 10):void => {
-    console.log(a + b)
-    console.log(c)
+const greetAgainOne = (user: objWithName) => {
+    console.log(`${user.name} says hello`)
 }
-
-addThree(5,10)
-addThree(5,10,20)
-addThree(5, 10, '20')
-
-const minus = (a: number, b: number) => {
-    return a + b;
-}
-
-let result = minus(10, 7)
-// result = 'soething else'
-
-// src/sandbox.ts:53:1 - error TS2322: Type 'string' is not assignable
-// to type 'number'.
-
-// 53 result = 'soething else'
-
-
-const minusOne = (a: number, b: number):number => {
-    return a + b;
-}
-
-let resultOne = minusOne(10, 7)
