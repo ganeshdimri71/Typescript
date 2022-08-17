428bc84f87a7d6e9fcf30a001925389fc11a92a0
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-17 23:33:23 +0530

    tuples

--- public/app.js
+++ public/app.js
@@ -1,43 +1,47 @@
-"use strict";
-// Generics
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docOne = addUID({ name: 'Ganesh', age: 40 });
-console.log(docOne);
-// console.log(docOne.name)
-// src/app.ts:11:20 - error TS2339: Property 'name' does not exist on type '{ uid: number; }'.
-// 11 console.log(docOne.name)
-const addUIDOne = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docTwo = addUIDOne({ name: 'Ganesh', age: 40 });
-console.log(docTwo.name);
-const addUIDTwo = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docThree = addUIDTwo({ name: 'Ganesh', age: 40 });
-console.log(docThree.name);
-// enum
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
-})(ResourceType || (ResourceType = {}));
-const docFour = {
-    uid: 1,
-    resourceName: ResourceType.BOOK,
-    data: { name: 'Ganesh' }
-};
-const docFive = {
-    uid: 1,
-    resourceName: ResourceType.PERSON,
-    data: ['test', 'bread', 'milk']
-};
-console.log(docFour, docFive);
+import { Invoice } from "./classes/invoice";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/payments";
+const form = document.querySelector('form');
+const formOne = document.querySelector('.new-item-form');
+const formTwo = document.querySelector('.new-item-form');
+console.log(formTwo.children);
+// inputs
+const type = document.querySelector('#type');
+const typeTwo = document.querySelector('#tofrom');
+const typeThree = document.querySelector('#details');
+const typeFour = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let values;
+    values = [typeTwo.value, typeThree.value, typeFour.valueAsNumber];
+    // let values = [typeTwo.value, typeThree.value,  typeFour.valueAsNumber]
+    let doc;
+    if (type.value === 'invoice') {
+        // doc = new Invoice(...values)
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
+});
+// tuples
+let arr = ['Ganesh', 25, true];
+arr[0] = false;
+arr[1] = 'Ganesh';
+arr = [30, false, 'Ganesh'];
+let tup = ['Ganesh', 35, true];
+// let tupOne: [string, number, boolean] = [35, 'Ganesh', true];
+// src/app.ts:9:46 - error TS2322: Type 'string' is not assignable to type 'number'.
+// 9 let tupOne: [string, number, boolean] = [35, 'Ganesh', true];
+let tupTwo = ['Ganesh', 35, true];
+tupTwo[0] = 'Sanjay';
+tupTwo[1] = 30;
+let student;
+student = ['Ganesh', 40];
+// student = [44859, ' Fansh']
+// src/app.ts:19:18 - error TS2322: Type 'string' is not assignable to type 'number'.
+// 19 student = [44859,' Fansh']
--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,60 @@
-// Generics
+import { Invoice } from "./classes/invoice";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/payments";
+import { HasFormatter } from "./interfaces/HasFormatter";
 
-const addUID = (obj: object) => {
-    let uid = Math.floor(Math.random() * 100);
-    return { ...obj, uid };
-}
+const form = document.querySelector('form')!;
+const formOne = document.querySelector('.new-item-form')!;
+const formTwo = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(formTwo.children);
 
-let docOne = addUID({name: 'Ganesh', age: 40})
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement
+const typeTwo = document.querySelector('#tofrom') as HTMLInputElement
+const typeThree = document.querySelector('#details') as HTMLInputElement
+const typeFour = document.querySelector('#amount') as HTMLInputElement
 
-console.log(docOne)
-// console.log(docOne.name)
-// src/app.ts:11:20 - error TS2339: Property 'name' does not exist on type '{ uid: number; }'.
-// 11 console.log(docOne.name)
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
 
-const addUIDOne = <T>(obj: T) => {
-    let uid = Math.floor(Math.random() * 100);
-    return { ...obj, uid };
-}
+    let values: [string, string, number]
+     values = [typeTwo.value, typeThree.value,  typeFour.valueAsNumber]
+    // let values = [typeTwo.value, typeThree.value,  typeFour.valueAsNumber]
 
-let docTwo = addUIDOne({name: 'Ganesh', age: 40})
+    let doc: HasFormatter;
+    if (type.value === 'invoice') {
+        // doc = new Invoice(...values)
+        doc = new Invoice(...values)
 
-console.log(docTwo.name)
+    } else {
+        doc = new Payment(...values)
+    }
 
-const addUIDTwo = <T extends {name: string}>(obj: T) => {
-    let uid = Math.floor(Math.random() * 100);
-    return { ...obj, uid };
-}
+    list.render(doc, type.value, 'end')
+    
+})
 
-let docThree = addUIDTwo({name: 'Ganesh', age: 40})
+// tuples
 
-console.log(docThree.name)
+let arr = ['Ganesh', 25, true];
+arr[0] = false;
+arr[1] = 'Ganesh'
+arr = [30, false, 'Ganesh']
 
+let tup: [string, number, boolean] = ['Ganesh', 35, true];
+// let tupOne: [string, number, boolean] = [35, 'Ganesh', true];
+// src/app.ts:9:46 - error TS2322: Type 'string' is not assignable to type 'number'.
+// 9 let tupOne: [string, number, boolean] = [35, 'Ganesh', true];
 
-// enum
+let tupTwo: [string, number, boolean] = ['Ganesh', 35, true];
+tupTwo[0] = 'Sanjay'
+tupTwo[1] = 30
 
-enum ResourceType {BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
-interface Resource<T>{
-    uid: number;
-    resourceName: ResourceType;
-    data: T; 
-}
-
-const docFour: Resource<object> = {
-    uid: 1,
-    resourceName: ResourceType.BOOK,
-    data: { name:'Ganesh'}
-}
-
-const docFive: Resource<string[]>  = {
-    uid: 1,
-    resourceName: ResourceType.PERSON,
-    data: ['test', 'bread', 'milk']
-}
-
-console.log(docFour, docFive)
+let student: [string, number]
+student = ['Ganesh', 40]
+// student = [44859, ' Fansh']
+// src/app.ts:19:18 - error TS2322: Type 'string' is not assignable to type 'number'.
+// 19 student = [44859,' Fansh']
