601134d9b57341c0ae5b494d2a237293177a59b0
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-17 21:03:26 +0530

    type alias in function

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,20 +1,31 @@
 "use strict";
-;
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid ${uid}`);
+//example 1
+let greet;
+// greet = (name: string, greeting: string) => {
+//     console.log(`${name} says ${greeting}`)
+// }
+// greet = (name: string, greeting: number) => {
+//     console.log(`${name} says ${greeting}`)
+// }
+// src/sandbox.ts:10:1 - error TS2322: Type '(name: string, greeting: number) => void' is not assignable to type '(a: string, b: string) => void'.
+//   Types of parameters 'greeting' and 'b' are incompatible.
+//     Type 'string' is not assignable to type 'number'.
+// 10 greet = (name: string, greeting: number) => {
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
+//example 1
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
-const greetAgain = (user) => {
-    console.log(`${user.name} says hello`);
-};
-const logDetailsOne = (uid, item) => {
-    console.log(`${item} has a uid ${uid}`);
-};
-const greetOne = (user) => {
-    console.log(`${user.name} says hello`);
-};
-const greetAgainOne = (user) => {
-    console.log(`${user.name} says hello`);
+//example 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,26 +1,41 @@
-type StringOrNum = string | number;;
-type objWithName = {name: string, id: StringOrNum}
 
-const logDetails = (uid: string | number, item: string) => {
-    console.log(`${item} has a uid ${uid}`)
+
+//example 1
+let greet: (a: string, b: string) => void
+
+// greet = (name: string, greeting: string) => {
+//     console.log(`${name} says ${greeting}`)
+// }
+
+// greet = (name: string, greeting: number) => {
+//     console.log(`${name} says ${greeting}`)
+// }
+// src/sandbox.ts:10:1 - error TS2322: Type '(name: string, greeting: number) => void' is not assignable to type '(a: string, b: string) => void'.
+//   Types of parameters 'greeting' and 'b' are incompatible.
+//     Type 'string' is not assignable to type 'number'.
+
+// 10 greet = (name: string, greeting: number) => {
+
+
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`)
 }
 
-const greet = (user: { name: string, uid: string | number }) => {
-    console.log(`${user.name} says hello`)
+//example 1
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo
+    }
 }
 
-const greetAgain = (user: { name: string, uid: string | number }) => {
-    console.log(`${user.name} says hello`)
-}
+//example 3
+let logDetails: (obj: {name: string, age: number }) => void;
 
-const logDetailsOne = (uid: StringOrNum, item: string) => {
-    console.log(`${item} has a uid ${uid}`)
-}
-
-const greetOne = (user:objWithName) => {
-    console.log(`${user.name} says hello`)
-}
-
-const greetAgainOne = (user: objWithName) => {
-    console.log(`${user.name} says hello`)
+type person = { name: string, age: number };
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`)
 }
