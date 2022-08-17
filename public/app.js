27796eef6d711d869d754729b5e8b6356706b245
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-17 22:32:01 +0530

    Interfaces

--- public/app.js
+++ public/app.js
@@ -1,9 +1,27 @@
-import { Invoice } from './classes/invoice.js';
-const invOne = new Invoice('Ganesh', 'work on the mario website', 250);
-const invTwo = new Invoice('Sanjay', 'work on the mario website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+"use strict";
+// Interfaces
+// const me: IsPerson = {}
+// src/app.ts:10:7 - error TS2739: Type '{}' is missing the following properties from type 'IsPerson': name, age, speak, spend
+// 10 const me: IsPerson = {}
+const me = {
+    name: 'Ganesh',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spend ', amount);
+        return amount;
+    },
+    //     skills: []
+    //     src/app.ts:25:5 - error TS2322: Type '{ name: string; age: number; speak(text: string): void; spend(amount: number): number; skills: never[]; }' is not assignable to type 'IsPerson'.
+    //   Object literal may only specify known properties, and 'skills' does 
+    // not exist in type 'IsPerson'.
+    // 25     skills: []
+};
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
+let someone;
+console.log(me);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,41 @@
-import {Invoice} from './classes/invoice.js'
-const invOne = new Invoice('Ganesh', 'work on the mario website', 250);
-const invTwo = new Invoice('Sanjay', 'work on the mario website', 300);
+// Interfaces
 
-let invoices: Invoice[] = [];
+interface IsPerson {
+    name: string,
+    age: number,
+    speak(a: string): void;
+    spend(a: number): number;
+}
 
-invoices.push(invOne)
-invoices.push(invTwo)
+// const me: IsPerson = {}
+// src/app.ts:10:7 - error TS2739: Type '{}' is missing the following properties from type 'IsPerson': name, age, speak, spend
 
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format())
-})
+// 10 const me: IsPerson = {}
+
+const me: IsPerson = {
+    name: 'Ganesh',
+    age: 30,
+    speak(text: string): void {
+        console.log(text)
+    },
+    spend(amount: number): number {
+        console.log('I spend ', amount)
+        return amount
+    },
+//     skills: []
+//     src/app.ts:25:5 - error TS2322: Type '{ name: string; age: number; speak(text: string): void; spend(amount: number): number; skills: never[]; }' is not assignable to type 'IsPerson'.
+//   Object literal may only specify known properties, and 'skills' does 
+// not exist in type 'IsPerson'.
+
+// 25     skills: []
+}
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello ', person.name )
+}
+
+greetPerson(me)
+
+let someone: IsPerson;
+
+console.log(me)
