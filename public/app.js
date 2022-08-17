b0850c45d6a632c10ab5695c5b603d5bc46bd6fb
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-17 22:23:22 +0530

    exports and imports

--- public/app.js
+++ public/app.js
@@ -1,23 +1,4 @@
-"use strict";
-class Invoice {
-    constructor(
-    //            client: string,
-    //            details: string,
-    //         amount: number,
-    // src/app.ts:28:21 - error TS2339: Property 'client' does not exist on type 'Invoice'.
-    // 28     console.log(inv.client, inv.amount, inv.format())
-    //                        ~~~~~~
-    // src/app.ts:28:33 - error TS2339: Property 'amount' does not exist on type 'Invoice'.
-    // 28     console.log(inv.client, inv.amount, inv.format())
-    client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice('Ganesh', 'work on the mario website', 250);
 const invTwo = new Invoice('Sanjay', 'work on the mario website', 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,12 @@
-class Invoice {
-
-    constructor(
-//            client: string,
-//            details: string,
-//         amount: number,
-           
-// src/app.ts:28:21 - error TS2339: Property 'client' does not exist on type 'Invoice'.
-
-// 28     console.log(inv.client, inv.amount, inv.format())
-//                        ~~~~~~
-
-// src/app.ts:28:33 - error TS2339: Property 'amount' does not exist on type 'Invoice'.
-
-// 28     console.log(inv.client, inv.amount, inv.format())
-          readonly client: string,
-          private details: string,
-          public amount: number,
-    ) { }
-
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`
-    }
-}
-
+import {Invoice} from './classes/invoice.js'
 const invOne = new Invoice('Ganesh', 'work on the mario website', 250);
 const invTwo = new Invoice('Sanjay', 'work on the mario website', 300);
-
 
 let invoices: Invoice[] = [];
 
 invoices.push(invOne)
 invoices.push(invTwo)
 
-
 invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format())
 })
-
-
-
-
